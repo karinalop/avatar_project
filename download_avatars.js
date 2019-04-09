73b659d113587db6c321717d026303cd10d23648
karinalop <karina.popeye@gmail.com>
2019-04-09 06:36:51 +0000

    Step 6

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,57 @@
 var request = require('request');
 var fs = require('fs');
+var token = require('./secrets.js');
 
 var args = process.argv;
 //var url = "https://github.com/" + args[2] + "/" + args[3];
 var url = "https://api.github.com/repos/jquery/jquery/contributors";
 console.log(url);
 
+function getRepoContributors(repoOwner, repoName, cb) {
+   var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization: 'Bearer ' + token.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
 
-
-
-request.get(url)               // Note 1
+/*
+function getRepoContributors(url,callback){
+  request.get(url)               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err;
        })
-       .on('response', function (response) {                           // Note 3
-         //var contributors = JSON.parse(response);
-         console.log(response);
+       .on('response', function (response) {
+        console.log(response);
+        var contributors = JSON.parse(response);
+        callback(contributors);
 
        })
        .pipe(fs.createWriteStream('./object.txt'));               // Note 4
+}
+*/
+//-------------------------------------------------------
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
+
+//-------------------------------------------------------
+
+function printAvatarURL(contributors){
+
+  console.log(contributors);
+
+  for(var i = 0; i <contributors.length; i++){
+    console.log(contributors.avatar_url);
+  }
+}
+
+//getRepoContributors(url);
 
